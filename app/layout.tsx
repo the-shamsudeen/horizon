60b719b7b956108fd4d6784e237e09d3c5734aca
the-shamsudeen <imuhammadshamsudeen@gmail.com>
2025-08-12 12:00:46 +0100

    add force-dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
-import {Inter, IBM_Plex_Serif } from "next/font/google";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -9,17 +11,16 @@ const inter = Inter({
 
 const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
-  weight: ['400', '700'],
-  variable: "--font-ibm-plex-serif"
-
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
 });
 
 export const metadata: Metadata = {
   title: "Horizon",
   description: "Horizon is a modern banking platform for everyone.",
   icons: {
-    icon: '/icons/logo.svg'
-  }
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
